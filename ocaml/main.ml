ebcd6df9be70c09c49c7ebfcf8adcd4f40871fb5
Frantisek Farka <frantisek@farka.eu>
2018-03-01 22:41:22 +0000

    main.ml

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -10,8 +10,8 @@ let () =
     let inBuffer = open_in filename in
     let lineBuffer = Lexing.from_channel inBuffer in 
     try
-        let ix = Parser.ix_start Lexer.token lineBuffer in
-            print_string (Parser_pp.pp_ix ix); print_newline(); 
+        let value = Parser.esgn_start Lexer.token lineBuffer in
+            print_string (Parser_pp.pp_esgn value); print_newline(); 
     with
         | Lexer.Error msg -> Printf.fprintf stderr "%s%!\n" msg
         | Parser.Error -> Printf.fprintf stderr "At offset %d: syntax error.\n%!" (Lexing.lexeme_start lineBuffer)
