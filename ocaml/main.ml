8eba189a90611ac0f364b072b53bc170993eada8
Frantisek Farka <frantisek@farka.eu>
2018-03-08 17:36:14 +0000

    prog options

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -10,20 +10,61 @@ open Specif
 open Defns
 open Refin
 
-let filename = Sys.argv.(1)
-let term = (Termstar_nl_pi_intro (Typestar_nl_mtvar 0 , Termstar_nl_mvar 1))
+open Helpers
 
-let pp_goal g t =
+let read_signature_filename_opt = ref (None : string option)
+let read_term_filename_opt = ref (None : string option)
+let write_basename_opt = ref (None : string option)
+let print_sig = ref false
+let print_term = ref false
+
+let options = Arg.align [
+    ( "-sig", 
+      Arg.String (fun s -> 
+        match !read_signature_filename_opt with
+        | None -> read_signature_filename_opt  := Some s
+        | Some _ -> Helpers.error "\nError: multiple -sig <filename> not suppported\n"),
+        "<filename>        Input signature" );
+    ( "-term", 
+      Arg.String (fun s -> 
+        match !read_term_filename_opt with
+        | None -> read_term_filename_opt  := Some s
+        | Some _ -> Helpers.error "\nError: multiple -term <filename> not suppported\n"),
+        "<filename>        Input term" );
+    ( "-o", 
+      Arg.String (fun s -> 
+        match !write_basename_opt with
+        | None -> write_basename_opt  := Some s
+        | Some _ -> Helpers.error "\nError: multiple -o <filename> not suppported\n"),
+        "<basename>        Output files basename" );
+  ( "-print-sig",
+    Arg.Bool (fun b -> print_sig := b),
+    "<"^string_of_bool !print_sig^">  Print parsed signature" );
+  ( "-print-term",
+    Arg.Bool (fun b -> print_term := b),
+    "<"^string_of_bool !print_sig^">  Print parsed term" );
+  ( "-print",
+    Arg.Bool (fun b -> print_sig := b; print_term := b),
+    "<"^string_of_bool (!print_sig && !print_term) ^">  Print parsed signature and term" );
+]
+
+let usage_msg =
+    ("\n"
+     ^ "usage: slepice -sig <filename> -term <filenamen> [ -o <basename> ]\n"
+    )
+
+
+
+
+let pp_goal g =
     match g with
-    | Coq_inleft (foo, _) -> "The goal for term >>" ^ Parser_pp.pp_ete t ^
-        "<< is: " ^ "\n\n\t" ^  Parser_pp.pp_tTGoal foo 
-    | Coq_inright -> "The term >>" ^ Parser_pp.pp_ete t ^ "<< cannot be refined"
+    | Coq_inleft (foo, _) -> Parser_pp.pp_tTGoal foo 
+    | Coq_inright -> error "The term cannot be refined"
 
 let pp_prog p =
     match p with
-    | Coq_inleft (foo, _) -> "The program is: " ^ "\n\n\t" ^ 
-        Parser_pp.pp_tTProg foo 
-    | Coq_inright -> "The signature cannot be refined"
+    | Coq_inleft (foo, _) -> Parser_pp.pp_tTProg foo 
+    | Coq_inright -> error "The signature cannot be refined"
 
 let parseVal fn
     (vp : (Lexing.lexbuf -> token) -> Lexing.lexbuf -> 'a) =
@@ -32,24 +73,61 @@ let parseVal fn
     try
         vp (Lexer.token) (lineBuffer)
     with
-        | Lexer.Error msg -> raise (Failure (Printf.sprintf "%s: %s%!\n" fn msg))
-        | Parser.Error -> raise (Failure (Printf.sprintf "%s, at offset %d: syntax error.\n%!"
-            fn (Lexing.lexeme_start lineBuffer))) 
+        | Lexer.Error msg -> error (Printf.sprintf "in %s, %s%!\n" fn msg)
+        | Parser.Error -> error (Printf.sprintf "in %s, at offset %d: syntax error.\n%!"
+            fn (Lexing.lexeme_start lineBuffer)) 
         
 ;;
 
+
+let _ =
+    let arguments = ref [] in
+      Arg.parse options
+        (fun s -> arguments := s :: !arguments)
+        usage_msg
+;;
+
+
+let input_signature_opt = 
+    Datatypes.option_map
+        (fun fn -> parseVal fn Parser.esgn_start)
+        !read_signature_filename_opt
   
+let input_term_opt = 
+    Datatypes.option_map
+        (fun fn -> parseVal fn Parser.ete_start)
+        !read_term_filename_opt
+
+let output_prog_ch =
+  match !write_basename_opt with
+    | Some bn -> open_out (bn ^ ".prog") 
+    | None -> stdout
+  
+let output_goal_ch =
+  match !write_basename_opt with
+    | Some bn -> open_out (bn ^ ".g") 
+    | None -> stdout
+
+let process_print_sig s =
+    if !print_sig then
+        Printf.printf "\n---\nParsed signature:\n%s\n\n---\n\n" (Parser_pp.pp_esgn s)
+    else ()
+
+let process_print_term t =
+    if !print_term then
+        Printf.printf "\n---\nParsed term:\n%s\n\n---\n\n" (Parser_pp.pp_ete t)
+    else ()
 
 let () =
-        let sigma = parseVal (Sys.argv.(1)) (Parser.esgn_start) in 
-        let t = parseVal (Sys.argv.(2)) (Parser.ete_start) in 
-        let g = Refin.goalterm_dec sigma [] t 0 in
-        let p = Refin.progsig_dec sigma 0 in
-            print_string (Parser_pp.pp_esgn sigma);
-            print_newline(); 
-            print_string (pp_prog p);
-            print_newline(); 
-            print_string (pp_goal g t);
-            print_newline();
-            print_newline()
-    ;;
+    match input_signature_opt , input_term_opt with
+      | None, _ -> error "No input signature provided .."
+      | _, None -> error "No input term provided .."
+      | (Some s , Some t) -> 
+            let g = Refin.goalterm_dec s [] t 0 in
+            let p = Refin.progsig_dec s 0 in
+                process_print_sig s;
+                process_print_term t;
+                Printf.fprintf output_prog_ch "%s\n" (pp_prog p);
+                Printf.fprintf output_goal_ch "%s\n" (pp_goal g);
+
+
